b63ceefbb8ed8d0c7b9af751e166ec6384b89a5f
Mohamed Samir <robben.97@hotmail.com>
2022-07-29 20:31:31 +0200

    End of section 7

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -11,6 +11,7 @@ namespace API.DTOs
         [Required]
         public string Username { get; set; }
         [Required]
+        [MaxLength(8),MinLength(4)]
         public string Password { get; set; }
     }
 }
