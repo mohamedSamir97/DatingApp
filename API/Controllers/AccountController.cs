30c0b08b952f727575f1bf673bc4d3643953378f
Mohamed Samir <robben.97@hotmail.com>
2022-08-08 23:38:10 +0200

    End of section 11

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -59,6 +59,7 @@ namespace API.Controllers
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
             var user =await _context.Users
+                .Include(p=>p.Photos)
                 .SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
             if (user ==null)
             {
@@ -79,7 +80,9 @@ namespace API.Controllers
             return new UserDto
             {
                 Username = user.UserName,
-                Token = _tokenService.CreateToken(user)
+                Token = _tokenService.CreateToken(user),
+                PhotoUrl =  user.Photos.FirstOrDefault(x => x.IsMain)?.Url
+
             };
 
 
